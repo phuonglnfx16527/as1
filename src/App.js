76b01c50d824e47b609c444fde291a7715cbda35
Knox <phuongle6428@gmail.com>
2022-06-22 21:54:34 +0700

    Component Part 2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import { DISHES } from './shared/dishes';
 import React, { Component } from 'react';
 import MenuComponent from './components/MenuComponent';
 
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
   render() {
     return (
       <div className="App">
@@ -12,7 +20,7 @@ class App extends Component {
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
-        <MenuComponent />
+        <MenuComponent dishes={this.state.dishes} />
       </div>
     );
   }
