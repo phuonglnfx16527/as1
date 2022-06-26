21f46845935ab9ca7b8ff0f261879c06d5385a77
Knox <phuongle6428@gmail.com>
2022-06-26 19:11:59 +0700

    5 commit

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import dateFormat from "dateformat";
 
 class MenuComponent extends Component {
 
@@ -19,8 +20,12 @@ class MenuComponent extends Component {
       return (
         <Card>
           <CardBody>
-            <CardTitle>{staff.name}</CardTitle>
-            <CardText>{staff.description}</CardText>
+            <CardTitle>Họ và tên: {staff.name}</CardTitle>
+            <CardText>Ngày sinh: {dateFormat(staff.doB, "dd/mm/yyyy")}</CardText>
+            <CardText>Ngày vào công ty: {dateFormat(staff.startDate, "dd/mm/yyyy")}</CardText>
+            <CardText>Phòng ban: {staff.department.name}</CardText>
+            <CardText>Số ngày nghỉ còn lại: {staff.annualLeave}</CardText>
+            <CardText>Số ngày đã làm thêm: {staff.overTime}</CardText>
           </CardBody>
         </Card>
       );
